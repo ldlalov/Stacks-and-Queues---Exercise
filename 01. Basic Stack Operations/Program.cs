e957de7fef82d6187de87ae3e12310b8ea1ff324
Любомир Лалов <ldlalov@hotmail.com>
2022-04-09 09:10:12 +0300

    Add project files.

---
+++ 01. Basic Stack Operations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack1 = new Stack<int>(Console.ReadLine().Split(' ').Select(int.Parse));
+
+        }
+    }
+}
